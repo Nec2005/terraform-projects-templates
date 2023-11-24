e3201f8b99d8bfa9115a5bb935c5eaf152d39362
Nec2005 <necatiunal2005@gmail.com>
2023-11-24 17:43:50 +0000

    eks-fargate

---
+++ 07-eks-fargate/eks-cluster.tf
@@ -0,0 +1,88 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = local.names.eks_cluster
+  version = local.var.cluster_version
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+  
+vpc_config {
+    subnet_ids = aws_subnet.public.*.id
+    public_access_cidrs     = ["0.0.0.0/0"]
+    
+    }  
+   
+timeouts {
+    delete    = "30m"
+    }
+
+depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy1,
+    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController1,
+    ]
+}
+
+## Attach roles to cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${local.names.eks_cluster}-cluster-role"
+  description = "Allow cluster to manage fargate nodes and cloudwatch logs"
+  force_detach_policies = true
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "eks.amazonaws.com",
+          "eks-fargate-pods.amazonaws.com"
+          ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy1" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    role       = aws_iam_role.eks_cluster_role.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController1" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+    role       = aws_iam_role.eks_cluster_role.name
+}
+
+##Add CloudWatch metrics to this cluster
+resource "aws_iam_policy" "AmazonEKSClusterCloudWatchMetricsPolicy" {
+  name   = "AmazonEKSClusterCloudWatchMetricsPolicy"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "cloudwatch:PutMetricData"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        }
+    ]
+}
+EOF
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKSCloudWatchMetricsPolicy" {
+  policy_arn = aws_iam_policy.AmazonEKSClusterCloudWatchMetricsPolicy.arn
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_cloudwatch_log_group" "cloudwatch_log_group" {
+  name              = "/aws/eks/${local.prefix}/cluster"
+  retention_in_days = 30
+
+  tags = {
+    Name        = "${local.prefix}-eks-cloudwatch-log-group"
+  }
+}
---
+++ 07-eks-fargate/eks-fargate.tf
@@ -0,0 +1,55 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "my-cluster"
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = var.vpc_id
+  subnet_ids               = var.subnet_ids
+
+
+  # Fargate Profile(s)
+  fargate_profiles = {
+    default = {
+      name = "default"
+      selectors = [
+        {
+          namespace = "default"
+        }
+      ]
+    }
+  }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = aws_iam_role.eks_admin_access_role.arn
+      username = aws_iam_role.eks_admin_access_role.name
+      groups   = ["devops-interview"]
+    },
+  ]
+
+  aws_auth_users = local.iam.eks_aws_auth_users
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
