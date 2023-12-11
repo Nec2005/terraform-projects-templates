1b795439ad099fed31b224708f969a530c48b856
Nec2005 <necatiunal2005@gmail.com>
2023-12-11 14:42:58 +0000

    k8s-deployment

---
+++ 12-k8s-resources-tf/c3-providers.tf
@@ -0,0 +1,31 @@
+# Terraform AWS Provider Block
+provider "aws" {
+  region = "eu-west-2"
+  profile = "terraform"
+
+}
+
+# Terraform Remote State Datasource
+data "terraform_remote_state" "eks" {
+  backend = "local"
+  config = {
+    path = "../11-eks-basic-nodes-private-public/terraform.tfstate"
+   }
+}
+
+
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+# Terraform Kubernetes Provider
+provider "kubernetes" {
+  host = data.terraform_remote_state.eks.outputs.cluster_endpoint 
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  token = data.aws_eks_cluster_auth.cluster.token
+}
